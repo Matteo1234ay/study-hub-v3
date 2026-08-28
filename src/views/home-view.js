fed1103536f4164a6f406b82f2dd0b671200cc4f
Matteo1234ay <matteo.paredi0@gmail.com>
2026-08-28 23:45:19 +0200

    chore: align home view imports with v22

--- src/views/home-view.js
+++ src/views/home-view.js
@@ -1,14 +1,14 @@
-import { PATHS, findLesson } from "../config/paths.js?v=20260828-21";
-import { element } from "../ui/components.js?v=20260828-21";
-import { createStudyStore } from "../study/study-store.js?v=20260828-21";
-import { createProgressStore } from "../progress/local-progress.js?v=20260828-21";
-import { createNotesStore } from "../study/notes-store.js?v=20260828-21";
-import { createReviewConceptsStore } from "../study/review-concepts-store.js?v=20260828-21";
+import { PATHS, findLesson } from "../config/paths.js?v=20260828-22";
+import { element } from "../ui/components.js?v=20260828-22";
+import { createStudyStore } from "../study/study-store.js?v=20260828-22";
+import { createProgressStore } from "../progress/local-progress.js?v=20260828-22";
+import { createNotesStore } from "../study/notes-store.js?v=20260828-22";
+import { createReviewConceptsStore } from "../study/review-concepts-store.js?v=20260828-22";
 import {
   createHomeQuickActions,
   createHomeStations
-} from "../home/home-stations.js?v=20260828-21";
-import { mountHomeExperience } from "../home/home-experience.js?v=20260828-21";
+} from "../home/home-stations.js?v=20260828-22";
+import { mountHomeExperience } from "../home/home-experience.js?v=20260828-22";
 
 function stationCaption(station, index) {
   return element("a", {
