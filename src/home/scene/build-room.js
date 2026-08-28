b2b0589ca407dd477a3fee630ff06ac59eacf12d
Matteo1234ay <matteo.paredi0@gmail.com>
2026-08-28 21:23:52 +0200

    chore: align room screen import with v21

--- src/home/scene/build-room.js
+++ src/home/scene/build-room.js
@@ -1,4 +1,4 @@
-import { createStationScreen } from "./screen-ui.js?v=20260828-20";
+import { createStationScreen } from "./screen-ui.js?v=20260828-21";
 
 const OPENING_CAMERA = Object.freeze({
   position: [-5.6, 3.05, 5.75],
